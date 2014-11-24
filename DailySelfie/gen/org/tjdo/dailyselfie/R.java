8252b5a08c63b7f4e06941ceabd4d5ed8abc3ff7
efmcuiti <efmcuiti@gmail.com>
2014-11-24 14:06:37 -0500

    Added basic functionality for picture taking using default camera app.

--- DailySelfie/gen/org/tjdo/dailyselfie/R.java
+++ DailySelfie/gen/org/tjdo/dailyselfie/R.java
@@ -22,21 +22,30 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_audio_alarm=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_alarm=0x7f080003;
+        public static final int action_camera=0x7f080002;
+        public static final int selfieName=0x7f080001;
+        public static final int thumbnail=0x7f080000;
     }
     public static final class layout {
         public static final int activity_daily_selfie=0x7f030000;
+        public static final int new_selfie_row=0x7f030001;
     }
     public static final class menu {
         public static final int daily_selfie=0x7f070000;
     }
     public static final class string {
+        public static final int action_alarm=0x7f050006;
+        public static final int action_camera=0x7f050005;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int empty_list=0x7f050001;
+        public static final int selfie_name=0x7f050004;
+        public static final int thumb_desc=0x7f050003;
     }
     public static final class style {
         /** 
--- DailySelfie/src/org/tjdo/dailyselfie/DailySelfieActivity.java
+++ DailySelfie/src/org/tjdo/dailyselfie/DailySelfieActivity.java
@@ -1,20 +1,59 @@
+/**
+ * DailySelfieActivity.java
+ * Created on: November, 2014
+ * For the sole purpose of the Just DO! Usage.
+ * All rights reserved.
+ */
 package org.tjdo.dailyselfie;
 
-import android.app.Activity;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class DailySelfieActivity extends Activity {
+/**
+ * Main activity for the last project in the Android Handheld devices 
+ * basic certification.
+ * @author efmcuiti [efmcuiti@gmail.com]
+ *
+ */
+public class DailySelfieActivity extends ListActivity {
+	
+	/** Defines the parameter to be passed to the camera application. */
+	private static final int REQUEST_IMAGE_CAPTURE = 1;
+	
+	/** Used to identify messages on logs. */
+	private static final String TAG = "TJDO-DailySelfie";
+	
+	/** Used to temporal storage of the new selfie. */
+	private String mCurrentImagePath;
 
+    /* (non-Javadoc)
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 1. Setting the customized layout list view.
         setContentView(R.layout.activity_daily_selfie);
     }
 
 
+    /* (non-Javadoc)
+     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -22,15 +61,83 @@ public class DailySelfieActivity extends Activity {
         return true;
     }
 
+    /* (non-Javadoc)
+     * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_camera) {
+        	dispatchTakePicture();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+	/**
+	 * Executes the camera application for photo taking.
+	 */
+	private void dispatchTakePicture() {
+		Intent takePic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		if (takePic.resolveActivity(getPackageManager()) != null) {
+			// 1. Configuring the path where the actual picture will be stored.
+			File imgFile = null;
+			
+			try {
+				imgFile = createImageFile();
+				// Updating the current image path.
+				mCurrentImagePath = "file:" + imgFile.getAbsolutePath();
+			} catch (IOException e) {
+				// Error saving image files.
+				Log.e(TAG, String.format("Couldn't create temporal image file!"), e);
+				return;
+			}
+			
+			// 2. Setting where to save the picture and starting the intent.
+			if (imgFile != null) {
+				Log.i(TAG, String.format("The new picture will be saved here %s", mCurrentImagePath));
+				takePic.putExtra(MediaStore.EXTRA_OUTPUT, 
+						Uri.fromFile(imgFile));
+				startActivityForResult(takePic, REQUEST_IMAGE_CAPTURE);
+			}
+		}
+	}
+	
+	/**
+	 * Builds a temporal file where the new image will be stored.
+	 * @return The temporal file.
+	 * @throws IOException When the operation can't be performed.
+	 */
+	private File createImageFile() throws IOException {
+		// The new filename is created from a date time stamp.
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+		String imageFileName = "SELFIE_" + timeStamp + "_";
+		File storageDir = getExternalFilesDir(null);
+		
+		// Building the answer object.
+		File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+		return image;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// Getting the taken image.
+		if ((requestCode == REQUEST_IMAGE_CAPTURE) && (resultCode == RESULT_OK)) {
+			if (data == null) {
+				Log.e(TAG, "Fatal error, data is null.");
+				return;
+			}
+			Bundle extras = data.getExtras();
+			Bitmap thumbnail = (Bitmap) extras.get("data");
+			Log.i(TAG, String.format("Selfie taken! %s", thumbnail.getByteCount()));
+		}
+	}
 }
